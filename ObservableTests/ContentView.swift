e793b222785d3673aa02116cae90713583e7fc38
Oskar Groth <oskar@cindori.org>
2023-09-12 12:54:27 +0200

    Initial Commit

---
+++ ObservableTests/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableTests
+//
+//  Created by Oskar Groth on 2023-09-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableTests/ObservableTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableTestsApp.swift
+//  ObservableTests
+//
+//  Created by Oskar Groth on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct ObservableTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
